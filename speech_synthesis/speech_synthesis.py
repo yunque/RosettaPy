79c715027d070f01a24a4f58f904b55bddbe42c6
IMR <i.martinez08@alumni.imperial.ac.uk>
2016-09-20 15:25:41 +0100

    Added info + link of submission to rosettacode.org

--- speech_synthesis/speech_synthesis.py
+++ speech_synthesis/speech_synthesis.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+'''Computer says it was all a dream.
+
+Submitted to Rosetta Code @ 160920
+http://rosettacode.org/wiki/Speech_synthesis#Python
+'''
+
 import pyttsx
 
 engine = pyttsx.init()
